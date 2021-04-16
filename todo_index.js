1475f5af948aa93eaafc6606cfdf7677648da277
prvnlhr <praveenlohar04@gmail.com>
2021-04-16 17:22:32 +0530

    added screenshots

--- todo_index.js
+++ todo_index.js
@@ -63,5 +63,5 @@ app.listen(port, function (err) {
   if (err) {
     console.log("Error in running the server", err);
   }
-  console.log("Server is running");
+  console.log("Server is running on port ::",port);
 });
